07dadd0ca21c93ff28efe4a3cca474e44e3ebdce
coolflorentca <cooflorentca@gmailcom>
2026-02-07 00:54:48 -0500

    Initial commit - Lab AWS DevOps

---
+++ script.js
@@ -0,0 +1,3 @@
+﻿document.getElementById('timestamp').textContent = new Date().toLocaleString();
+document.getElementById('status').className = 'status-success';
+document.getElementById('status').textContent = ' Déployé avec succès via AWS DevOps';
